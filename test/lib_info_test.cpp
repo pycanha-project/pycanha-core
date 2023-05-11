0da8b02ee16758d078baf6ef3dd65696ac9584d9
ULTRAPCW11\Javier <javier.piqueras@upm.es>
2023-05-11 13:57:24 +0900

    Starting with a C++ template

---
+++ test/lib_info_test.cpp
@@ -0,0 +1,6 @@
+#include "pycanha-core/my_lib.hpp"
+
+int main() {
+  print_package_info();
+  return 0;
+}
---
+++ test_package/test_package.cpp
@@ -0,0 +1,3 @@
+#include "pycanha-core/my_lib.hpp"
+
+int main() { print_package_info(); }
