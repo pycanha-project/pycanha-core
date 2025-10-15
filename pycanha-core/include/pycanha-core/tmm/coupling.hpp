1ee348aabdcae9576eb29c20dc2a2450f1b042f0
Javier <javier.piqueras@upm.es>
2025-10-15 19:29:44 +0200

    Using Index instead of int

--- pycanha-core/include/pycanha-core/tmm/coupling.hpp
+++ pycanha-core/include/pycanha-core/tmm/coupling.hpp
@@ -3,24 +3,26 @@
 
 #include <Eigen/Sparse>
 
+#include "pycanha-core/parameters.hpp"
+
 namespace pycanha {
 
 /// Representation of a thermal coupling between two nodes.
 class Coupling {
   public:
     /// Construct a coupling between two user nodes with a given conductance.
-    Coupling(int node_1, int node_2, double value) noexcept;
+    Coupling(Index node_1, Index node_2, double value) noexcept;
 
-    [[nodiscard]] int get_node_1() const noexcept;
-    [[nodiscard]] int get_node_2() const noexcept;
+    [[nodiscard]] Index get_node_1() const noexcept;
+    [[nodiscard]] Index get_node_2() const noexcept;
     [[nodiscard]] double get_value() const noexcept;
 
-    void set_node_1(int node_1) noexcept;
-    void set_node_2(int node_2) noexcept;
+    void set_node_1(Index node_1) noexcept;
+    void set_node_2(Index node_2) noexcept;
     void set_value(double value) noexcept;
 
   private:
-    Eigen::Triplet<double> _conductor;
+    Eigen::Triplet<double, Index> _conductor;
 };
 
 }  // namespace pycanha
--- test/tmm/test_coupling.cpp
+++ test/tmm/test_coupling.cpp
@@ -1,12 +1,13 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include "pycanha-core/parameters.hpp"
 #include "pycanha-core/tmm/coupling.hpp"
 
 using namespace pycanha;  // NOLINT(build/namespaces)
 
 TEST_CASE("Coupling stores nodes and value", "[coupling]") {
-    const int node_1 = 2;
-    const int node_2 = 7;
+    const Index node_1 = 2;
+    const Index node_2 = 7;
     const double conductor_value = 5.0;
     Coupling coupling(node_1, node_2, conductor_value);
 
@@ -16,7 +17,7 @@ TEST_CASE("Coupling stores nodes and value", "[coupling]") {
     REQUIRE(conductor_value == coupling.get_value());
 
     SECTION("Updating first node keeps other attributes") {
-        const int new_node_1 = 17;
+        const Index new_node_1 = 17;
         coupling.set_node_1(new_node_1);
 
         REQUIRE(new_node_1 == coupling.get_node_1());
@@ -25,7 +26,7 @@ TEST_CASE("Coupling stores nodes and value", "[coupling]") {
     }
 
     SECTION("Updating second node keeps other attributes") {
-        const int new_node_2 = 29;
+        const Index new_node_2 = 29;
         coupling.set_node_2(new_node_2);
 
         REQUIRE(node_1 == coupling.get_node_1());
@@ -43,8 +44,8 @@ TEST_CASE("Coupling stores nodes and value", "[coupling]") {
     }
 
     SECTION("Updating all attributes reflects in getters") {
-        const int new_node_1 = 17;
-        const int new_node_2 = 29;
+        const Index new_node_1 = 17;
+        const Index new_node_2 = 29;
         const double new_value = 7.8;
 
         coupling.set_node_1(new_node_1);
