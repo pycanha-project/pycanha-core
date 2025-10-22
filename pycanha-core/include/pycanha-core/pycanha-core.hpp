fe31e087eb3da36f8701d4df7006d3d670a783d7
Javier <javier.piqueras@upm.es>
2025-10-22 11:20:49 +0200

    added parameters and thermal data

--- pycanha-core/include/pycanha-core/pycanha-core.hpp
+++ pycanha-core/include/pycanha-core/pycanha-core.hpp
@@ -1,13 +1,17 @@
 #pragma once
 
+// NOLINTBEGIN(misc-include-cleaner)
+
 // General config (not really used here)
-#include "./config.hpp"
+#include "./config.hpp"  // IWYU pragma: export
 
 // Utilities
-#include "./utils/package_info.hpp"
+#include "./utils/package_info.hpp"  // IWYU pragma: export
 
 // Modules:
-#include "./parameters.hpp"
+#include "./parameters.hpp"  // IWYU pragma: export
 
 // Geometric module
-#include "./gmm/gmm.hpp"
+#include "./gmm/gmm.hpp"  // IWYU pragma: export
+
+// NOLINTEND(misc-include-cleaner)
